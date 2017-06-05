275688c84a557be6ff5df8cd0323d94fb79fc85a
skyanycall123 <zy_3012@163.com>
2017-06-06 02:01:28 +0800

    第一个socket服务端程序

---
+++ ex_socket/server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#coding= gbk
+# file name server.py
+
+import socket
+
+s = socket.socket()
+host = '192.168.80.137'
+port=12345
+s.bind((host,port))
+
+
+s.listen(5)
+while True :
+    c,addr= s.accept()
+    print('连接地址:',addr)
+    c.send(('欢迎你，菜鸟!\n' +'收到请回答!').encode())
+    #c.send(('收到请应答!!!').encode())
+    recvdata=c.recv(1024)
+    print(recvdata.decode())
+    if (len(recvdata) >  0):
+        c.send(recvdata)
+    else :
+        c.send((' have not get the message !').encode())   
+    
+  
+
+
